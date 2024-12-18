642b0ee90f357cf100610de79b6c7972dab6fc26
Zack Sargent <zack@zack.fyi>
2024-12-17 22:28:33 -0500

    add scripts

---
+++ livestream/stream-to-twitch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+KEY=live_XXXXXXXXXXXXXXXXX
+
+function realSignal() {
+        ffmpeg -re -f v4l2 -i /dev/video0 -c:v libx264 -preset veryfast -b:v 6000k -maxrate 6000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv "rtmp://live-fra.twitch.tv/app/$KEY"
+}
+
+function testSignal() {
+        ffmpeg -re \
+                -f lavfi -i testsrc2=size=960x540 \
+                -f lavfi -i aevalsrc="sin(0*2*PI*t)" \
+                -vcodec libx264 \
+                -r 30 -g 30 \
+                -preset fast -vb 3000k -pix_fmt rgb24 \
+                -pix_fmt yuv420p \
+                -f flv \
+                rtmp://live-fra.twitch.tv/app/$KEY
+}
+
+realSignal
+# testSignal
+
---
+++ livestream/stream_to_youtube.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# https://gist.githubusercontent.com/olasd/9841772/raw/12c78a9426af9102e7c3fe40652c96f3c793aa9d/stream_to_youtube.sh
+#
+# Diffusion youtube avec ffmpeg
+
+VBR="2500k"
+FPS="30"
+QUAL="medium"
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
+
+SOURCE="/dev/video0"
+
+if [[ -z "$KEY" ]]; then
+    echo "Must provide KEY in environment" 1>&2
+    exit 1
+fi
+
+ffmpeg \
+    -i "$SOURCE" \
+    -vcodec  -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -b:a 712000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
