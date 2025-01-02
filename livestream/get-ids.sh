3a7bd2813da19a6941af0b5defa19f25d689fab5
Zack Sargent <zack@zack.fyi>
2025-01-02 13:32:57 -0500

    add link

--- livestream/get-ids.sh
+++ livestream/get-ids.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# using https://pypi.org/project/twitch-dl/
+# (pipx install twitch-dl)
+
 TWITCH_CHANNEL_NAME=norsecoderadio
 
 twitch-dl videos "$TWITCH_CHANNEL_NAME" --all | grep https | sed 's/videos\// /' | awk '{print $2}' > ids.txt
